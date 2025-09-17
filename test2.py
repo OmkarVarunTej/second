4a1c053e8097d2c028c0411b9d5853d45bed31bc
OmkarVarunTej <omkargudiguntlayt@gmail.com>
2025-09-17 20:26:44 +0530

    Added a new Feature in Feature3 branch

--- test2.py
+++ test2.py
@@ -1,9 +1,3 @@
 print("Testing")
-a = 7
-if a ==7 :
-    print("The Feature is Working")
-else:
-    print("The Feature is not Working")
-    
-print("Testing Feature 2")
+print("Testing Feature 3")
     
